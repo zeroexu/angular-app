55bf68c940be13d29fe4e433b59b0d6e52ae8419
Ali Lopez <ali.lopez.gonzalez@igam.io>
2020-02-05 10:33:09 -0300

    modules of auth sub components

---
+++ src/app/auth/recover/recover.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RecoverRoutingModule } from './recover-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RecoverRoutingModule
+  ]
+})
+export class RecoverModule { }
